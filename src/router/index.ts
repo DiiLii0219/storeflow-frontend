1aad13535ae6b11a03f9ed510803ff90c2a94291
DiiLii0219 <1598957152@qq.com>
2025-08-02 06:10:09 +0800

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', component: () => import('@/views/Home.vue') },
+  { path: '/login', component: () => import('@/views/Login.vue') },
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
